aa6c15aa2f01be416e74971e62f7de6ee81ce183
M Rizal Rifai <mrizxlrifxi@protonmail.com>
2022-05-19 23:09:58 +0700

    remove

--- .config/nvim/queries/cpp/textobjects.scm
+++
@@ -1,9 +0,0 @@
-; Swappable items
-(_) @node
-
-(argument_list (_) @swappable)
-(parameter_list (_) @swappable)
-(field_initializer_list (_) @swappable)
-(field_declaration_list (_) @swappable)
-(initializer_list (_) @swappable)
-(enumerator_list (_) @swappable)
--- .config/nvim/queries/lua/textobjects.scm
+++
@@ -1,6 +0,0 @@
-; Swappable items
-(_) @node
-
-(arguments (_) @swappable)
-(field) @swappable
-(parameters (_) @swappable)
